206ef72ba8101b6a3e2adc0e56c4067ba7e94f49
Andrew Beach <andrew@team.shoeboxed.com>
2017-07-23 19:49:42 -0400

    qualified name for temporary clojar

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject shoeboxed-api "0.1.0"
+(defproject org.clojars.andrewbeach/shoeboxed-api "0.1.0"
   :description "Wrapper for the public Shoeboxed HTTP API"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
