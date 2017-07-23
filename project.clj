fbb85eced8bd67c82bf07421d3c02e719307bf6a
Andrew Beach <andrew@team.shoeboxed.com>
2017-07-23 19:48:15 -0400

    updated project config

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject shoeboxed-api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject shoeboxed-api "0.1.0"
+  :description "Wrapper for the public Shoeboxed HTTP API"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
